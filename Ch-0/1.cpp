f9370f6850b7caf68bb108db2c7aa848e73ce0b9
Suman Chakrabortty <suman.chakrabortty8514@gmail.com>
2024-08-03 23:10:48 +0530

    New C++ Codes entry

---
+++ Ch-0/1.cpp
@@ -0,0 +1,14 @@
+// Code Topic: First project of c++//,
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "What is Your Name:" << endl;
+    char n[10];
+    cin >> n;
+    cout << "..............................." << endl;
+    cout << "Welcome " << n;
+
+    return 0;
+}
---
+++ Ch-0/2.cpp
@@ -0,0 +1,26 @@
+// Code Topic: revarse number//,
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Number to Revarse:" << endl;
+    cin >> n;
+    int r = 0;
+    cout << "Number in Revarse:";
+    while (n > 0)
+    { // Logic 1 starts
+        int t = n % 10;
+        r = r * 10 + t;
+        n /= 10;
+    }
+    cout << r; // Logic 1 ends
+
+    // {                //Logic 2 starts
+    //     int t = n % 10;
+    //     cout << t;
+    //     n /= 10;
+    // }                // Logic 2 ends
+
+    return 0;
+}
